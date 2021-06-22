fab04b5a5feec82f97ec250e810cad49d5a6b0a4
Harry Hu <hoozh1123@gmail.com>
2021-06-22 20:53:29 +1000

    feature(): Add Authentication.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+//dev-5a6tmbuw.au.auth0.com
+//2CDPh8U8ypgexJMZdbnwXQiskeHK6MhM
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-5a6tmbuw.au.auth0.com'
+      clientId='2CDPh8U8ypgexJMZdbnwXQiskeHK6MhM'
+      redirectUri={window.location.origin}
+      cacheLocation='localstorage'
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
